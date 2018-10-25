a6835782e88a28dc1e62a017d50de7ec139733b2
西瓜丶丶 <tomqin111@aliyun.com>
2018-10-25 12:54:55 +0800

    配置更新

--- src/root.js
+++ src/root.js
@@ -24,3 +24,10 @@ export default class App extends Component {
       )
   }
 }
+
+/*
+Provider组件是让所有的组件可以访问到store。不用手动去传。也不用手动去监听。
+
+connect函数作用是从 Redux state 树中读取部分数据，
+并通过 props 来把这些数据提供给要渲染的组件。也传递dispatch(action)函数到props。
+ */
